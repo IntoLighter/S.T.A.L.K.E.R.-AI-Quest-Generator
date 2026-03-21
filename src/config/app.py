5296d5e573601174eed3a9aa943f18fde4516d11
Андрей Алексеев <analekseev2016@yandex.com>
2026-03-21 23:37:40 +0700

    v1.1

--- src/config/app.py
+++ src/config/app.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class AppConfig(BaseSettings):
-    version: str = "1.0"
+    version: str = "1.1"
     name: str = "S.T.A.L.K.E.R. AI Quest Generator"
     repository: str = "https://github.com/IntoLighter/S.T.A.L.K.E.R.-AI-Quest-Generator"
 
