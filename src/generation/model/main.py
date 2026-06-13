4913fb06b97ba6e580c93ee0052c11b83cbe46d2
Intolighter <analekseev2016@yandex.com>
2026-06-13 15:06:02 +0700

    feat: specify retries count for openai client operations

--- src/generation/model/main.py
+++ src/generation/model/main.py
@@ -1,28 +1,33 @@
 ﻿from typing import Iterator
 
-import openai
 from loguru import logger
+from openai import OpenAI
 from openai.types.chat import ChatCompletionMessageParam
 
 
-class Model(openai.OpenAI):
+class Model:
     def __init__(self, model: str, base_url: str, api_key: str) -> None:
-        super().__init__(api_key=api_key, base_url=base_url)
+        self.client = OpenAI(api_key=api_key, base_url=base_url)
         self.model = model
 
     def generate(
-        self, messages: list[ChatCompletionMessageParam], **kwargs
+        self, messages: list[ChatCompletionMessageParam], retries: int = 2, **kwargs
     ) -> Iterator[str]:
-        stream = self.chat.completions.create(
+        stream = self.client.with_options(max_retries=retries).chat.completions.create(
             model=self.model, messages=messages, stream=True, **kwargs
         )
         for response in stream:
             yield response.choices[0].delta.content or ""
 
-    def get_models(self) -> list[str]:
+    def get_models(self, retries: int = 0) -> list[str]:
         # TODO: Move to service
         try:
-            return [model.id for model in self.models.list().data]
+            return [
+                model.id
+                for model in self.client.with_options(max_retries=retries)
+                .models.list()
+                .data
+            ]
         except Exception as e:
             logger.exception(e)
             return []
